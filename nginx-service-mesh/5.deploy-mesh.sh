ea9b067db365bb85d0267223895b1a230fe3bbcd
ubuntu <31039225+jessegoodier@users.noreply.github.com>
2021-01-26 02:34:29 +0000

    mesh8

--- nginx-service-mesh/5.deploy-mesh.sh
+++ nginx-service-mesh/5.deploy-mesh.sh
@@ -1,12 +1,13 @@
-tag=0.6.0
+tag=0.7.0
 registry=$1:444
 nginx-meshctl deploy --registry-server $registry \
     --nginx-mesh-api-image "nginx-mesh-api:$tag" \
     --nginx-mesh-sidecar-image "nginx-mesh-sidecar:$tag" \
     --nginx-mesh-init-image "nginx-mesh-init:$tag" \
     --nginx-mesh-metrics-image "nginx-mesh-metrics:$tag" \
-    --disabled-namespaces "kubernetes-dashboard" \
-    --enabled-namespaces "default"
+    --disable-auto-inject
+    #--disabled-namespaces "kubernetes-dashboard" \
+    #--enabled-namespaces "default"
     # --prometheus-address "k8s.nginx.rocks:9090" \
     # --persistent-storage "auto"
 
